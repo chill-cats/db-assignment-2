83ce700c05a8a26b2cea130745aa7137e6d4e43f
Nhân Lê Nguyễn <nhanlenguyen@PickleBuffer.local>
2022-11-30 12:48:35 +0700

    Add screen switching logic

--- App.py
+++ App.py
@@ -4,6 +4,9 @@ from screens.LoginScreen import LoginScreen
 import psycopg.errors as pg_errors
 import psycopg as pg
 import tomli
+import tkinter.messagebox as messagebox
+from enum import Enum
+from typing import Optional
 
 
 def load_config():
@@ -24,17 +27,30 @@ class App(tk.Tk):
         self.geometry('800x600')
         self.resizable(0, 0)
 
-        self.menu_screen = MenuScreen(self)
-        self.menu_screen.place(in_=self, x=0, y=0, relwidth=1, relheight=1)
+        self.sub_screens = [MenuScreen(self), LoginScreen(self)]
+        for screen in self.sub_screens:
+            screen.place(in_=self, x=0, y=0, relwidth=1, relheight=1)
 
-        self.login_screen = LoginScreen(self)
-        self.login_screen.place(in_=self, x=0, y=0, relwidth=1, relheight=1)
+        self.pg_connection: Optional[pg.Connection] = None
+
+    def switch_to(self, screen: str):
+        match screen:
+            case "MenuScreen":
+                self.sub_screens[0].tkraise()
+            case "LoginScreen":
+                self.sub_screens[1].tkraise()
+            case _:
+                raise ValueError(
+                    f"screen must be one of \"MenuScreen\" or \"LoginScreen\", recived {screen}")
 
     def login(self, username: str, password: str):
         print(f"Connecting with {username}, {password}")
         try:
-            pg_connection = pg.connect(
+            self.pg_connection = pg.connect(
                 f"host={self.app_config['postgres']['ip']} user={username} password={password}")
         except pg_errors.OperationalError as e:
-            print(e)
-        return None
+            err_message = str(e)
+            messagebox.showerror(
+                message=f"Cannot connect to database with error {err_message}")
+            return
+        self.switch_to("MenuScreen")
